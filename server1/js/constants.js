2d6ff6117c61f25eecab4150efbad650e49701fd
Brad Hultman <braddhultman@gmail.com>
2025-11-02 21:32:20 -0800

    Changing back

--- server1/js/constants.js
+++ server1/js/constants.js
@@ -1,7 +1,7 @@
 // API base URL for backend server (cannot use env vars in frontend)
 // export const BACKEND_SERVER_URL = 'http://localhost:3000/api';
 export const BACKEND_SERVER_URL =
-  "https://comp4537-group5-9lwth.ondigitalocean.app";
+  "https://comp4537-group5-9lwth.ondigitalocean.app/api";
 
 // Base URL for the standalone AI (text-to-speech) microservice
 export const AI_SERVER_URL = "http://localhost:8081/api/v1/tts";
