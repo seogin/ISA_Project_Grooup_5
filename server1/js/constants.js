b0a0d50e34181caa58f2f50ca85d900ee9a26f0c
seogin <hsihsj94@gmail.com>
2025-11-02 22:42:09 -0800

    Update AI_SERVER_URL to use secure HTTPS endpoint

--- server1/js/constants.js
+++ server1/js/constants.js
@@ -4,7 +4,7 @@ export const BACKEND_SERVER_URL =
   "https://comp4537-group5-9lwth.ondigitalocean.app/api";
 
 // Base URL for the standalone AI (text-to-speech) microservice
-export const AI_SERVER_URL = "http://localhost:8081/api/v1/tts";
+export const AI_SERVER_URL = "https://tree-fares-aluminium-bool.trycloudflare.com/api/v1/tts";
 
 const UI_STRINGS = {
   ERROR_UNAUTHORIZED:
